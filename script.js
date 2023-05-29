5842ace3a19e3e9f088d219db07e1b9c7aeed520
Bohdan <redliz.redliz@gmail.com>
2023-05-29 10:42:06 +0300

    Added game reset function

--- script.js
+++ script.js
@@ -10,29 +10,44 @@
 //   console.log(document.querySelector('.number-input').value);
 // };
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.question').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+// Game screen
+document.querySelector('.question').textContent = '???';
+
+// Game points
 let score = 20;
 
+// Game functionality
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   console.log(guessingNumber, typeof guessingNumber);
 
+  // Game end
   if (score > 1) {
+    // No input
     if (!guessingNumber) {
       document.querySelector('.guess-message').textContent = 'Введите число!';
       document.querySelector('body').style = 'rgb(0, 0, 0)';
+
+      // Play won
     } else if (guessingNumber === secretNumber) {
       document.querySelector('.guess-message').textContent = 'Правильно!';
-      // document.querySelector('.question').textContent = secretNumber;
-      document.querySelector('body').style =
-        'background-color: rgb(9, 250, 21)';
+      document.querySelector('.question').textContent = secretNumber;
+      document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
+      document.querySelector('.question').style.width = '50rem';
+
+      // Best score
       document.querySelector('.highscore').innerHTML = score;
+
+      // Too high
     } else if (guessingNumber > secretNumber) {
       document.querySelector('.guess-message').textContent = 'Сильшком много!';
       document.querySelector('body').style = 'rgb(0, 0, 0)';
       score--;
       document.querySelector('.score').textContent = score;
+
+      // Too low
     } else if (guessingNumber < secretNumber) {
       document.querySelector('.guess-message').textContent = 'Сильшком мало!';
       document.querySelector('body').style = 'rgb(0, 0, 0)';
@@ -43,4 +58,17 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.guess-message').textContent = 'Game over!';
     document.querySelector('.score').textContent = 0;
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
+  document.querySelector('.question').textContent = '???';
+  document.querySelector('.question').style.width = '25rem';
+  document.querySelector('.number-input').value = '';
+  document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+  document.querySelector('.score').textContent = score;
+  //   document.querySelector('.highscore').textContent = '0';
 });
